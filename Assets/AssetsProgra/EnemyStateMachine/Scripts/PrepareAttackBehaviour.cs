87ae456a7e815647108ee1d0ac75487a878d23a2
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-04-30 11:06:40 +0200

    Carrot 2

--- Assets/AssetsProgra/EnemyStateMachine/Scripts/PrepareAttackBehaviour.cs
+++ Assets/AssetsProgra/EnemyStateMachine/Scripts/PrepareAttackBehaviour.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class PrepareAttackBehaviour : BaseBehaviour
 {
-    private NavMeshAgent enemyNavmesh;
     protected float _timer;
     protected float prepareAttackTime;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
-        enemyNavmesh = animator.GetComponent<NavMeshAgent>();
-        enemyNavmesh.isStopped = true;
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
