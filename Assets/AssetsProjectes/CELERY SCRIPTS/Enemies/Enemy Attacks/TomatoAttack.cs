7958162334b90c778087b5e74e426a5e34839df4
lauraxiaoperis <160018903+lauraxiaoperis@users.noreply.github.com>
2024-04-30 22:21:47 +0200

    Tomato Attack good

--- Assets/AssetsProjectes/CELERY SCRIPTS/Enemies/Enemy Attacks/TomatoAttack.cs
+++ Assets/AssetsProjectes/CELERY SCRIPTS/Enemies/Enemy Attacks/TomatoAttack.cs
@@ -5,34 +5,67 @@ using UnityEngine.UI;
 
 public class TomatoAttack : Enemy
 {
-    //Animació provisional BORRAR quan tinguem les animacions
-    [Header("Animació provisional :)")]
-    public float attackScaleMultiplier = 1.5f;
-    private Vector3 originalScale;
     public Slider heealthSliderTomato;
+    private Collider attackCollider;
+    private Camera mainCamera;
+
+    [Header("Tomato Sphere Attack")]
+    public float sphereRadius = 0.6f;
+    public float distanceAhead = 0.9f; 
+    public float distanceAbove = 0.6f;
 
     void Start()
     {
         target = GameObject.FindGameObjectsWithTag("Player")[0];
-        originalScale = transform.localScale; // Borrar quan tinguem les animacions
+        mainCamera = Camera.main;
     }
+
     private void Update()
     {
         heealthSliderTomato.value = CurrentHealth;
+        // Orientar el slider a la camera
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0f; 
+        heealthSliderTomato.transform.rotation = Quaternion.LookRotation(cameraForward);
     }
+
     public override void Attack()
     {
-        Debug.Log(prepareAttackTime);
-        if (InRange())
-        {
-            Debug.Log("Tomato damage: " + damage);
-            Damager();
-        }
+        CreateAttackCollider();
     }
+
     public override void FacePlayer()
     {
         Vector3 direction = target.transform.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
         transform.rotation = rotation;
+    }
+
+    private void CreateAttackCollider()
+    {
+        if (attackCollider != null)
+        {
+            Destroy(attackCollider.gameObject);
+        }
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + transform.forward * distanceAhead + Vector3.up * distanceAbove, sphereRadius);
+
+        foreach (var collider in hitColliders)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                Damager();
+            }
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Breakables"))
+            {
+ 
+                collider.GetComponent<IDamageable>().TakeDamage(-damage);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + transform.forward * distanceAhead + Vector3.up * distanceAbove, sphereRadius);
     }
 }
